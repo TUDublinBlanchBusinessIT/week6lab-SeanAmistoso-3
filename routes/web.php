ea67d0af724ee5536c5b06078b3290931a1c3ea6
B00137752 Sean Amistoso <B00137752@mytudublin.ie>
2025-03-03 16:09:34 +0000

    Step 2 done

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,5 @@ Route::resource('scorders', App\Http\Controllers\scorderController::class);
 Route::resource('orderdetails', App\Http\Controllers\orderdetailController::class);
 
 Route::get('product/displaygrid', 'App\Http\Controllers\productController@displaygrid')->name('products.displaygrid');
+
+Route::get('product/additem/{id}', 'App\Http\Controllers\productController@additem')->name('products.additem');
